8504323ecf51cc8cc7d3d472853967a21a1f79de
Zorlock <joeleonti@gmail.com>
2020-11-02 18:41:57 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -39,14 +39,16 @@ var fight = function (enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 // Subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney)
                 break;
             }
         }
 
-        // Remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // Generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
         );
@@ -65,7 +67,9 @@ var fight = function (enemyName) {
         }
 
         // Remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
@@ -96,7 +100,7 @@ var startGame = function () {
             // Pick new enemy to fight based on the index of enemyNames array
             var pickedEnemyName = enemyNames[i];
 
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             // Reset enemyHealth before starting new fight
 
             // Pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
@@ -197,6 +201,13 @@ var shop = function () {
     }
 };
 
+
+// Function to generate a random numeric value
+var randomNumber = function (min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+};
 
 
 // Start the game when the page loads
