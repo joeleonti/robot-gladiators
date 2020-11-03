ac99540c605c19d46f4273655d13689b74894e7d
Zorlock <joeleonti@gmail.com>
2020-11-02 19:57:38 -0500

    spelling error

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,7 +15,7 @@ var playerInfo = {
         this.attack = 10;
     },
     refillHealth: function () {
-        if (this.monye >= 7) {
+        if (this.money >= 7) {
             window.alert("Refilling player's health by 20 for 7 dollars");
             this.health += 20;
             this.money -= 7;
