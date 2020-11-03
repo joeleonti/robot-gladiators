315b9696f1098a8f15742e19668a66f610eb0cb2
Zorlock <joeleonti@gmail.com>
2020-11-03 14:54:15 -0500

    messed up real good

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,9 +3,23 @@
 //    * Fight all enemy robots
 // "LOSE" - Player robot's health is is zero or less
 
+// Function to set name
+var getPlayerName = function () {
+    var name = "";
+
+   while (name === "" || name === null) {
+       name = prompt("What is your robot's name?");
+   }
+
+    console.log("Your robot's name is " + name);
+  return name;
+};
+
+
+
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
