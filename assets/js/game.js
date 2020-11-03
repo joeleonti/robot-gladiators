4bfb5460b9405f5565b397535299db0e221da516
Zorlock <joeleonti@gmail.com>
2020-11-03 16:09:55 -0500

    shop input fixed?

--- assets/js/game.js
+++ assets/js/game.js
@@ -210,28 +210,24 @@ var endGame = function () {
 
 var shop = function () {
     // Ask player what they'd like to do
-    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
 
     // Use switch to carry out action 
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-        case "REFILL": // New cae
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE": // New case    
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE": // New case
-        case "leave":
+        case 3:
             window.alert("Leaving the store");
-
             // Do nothing, so function will end
             break;
         default:
             window.alert("You did not pick a valid option. Try again.");
-
             // Call shop() again to force player to pick a valid option 
             shop();
             break;
@@ -264,6 +260,21 @@ var enemyInfo = [
 
 // Start the game when the page loads
 startGame();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
